766be2c06d612c4661ddea967676d65eff8cea78
Andrii Makovskyi <andrey.makovskiy@outlook.com>
2020-09-19 12:16:45 +0300

    Added docs for AppTransitionFactory

--- app/src/main/java/net/apptronic/core/demoapp/android/AppTransitionFactory.kt
+++ app/src/main/java/net/apptronic/core/demoapp/android/AppTransitionFactory.kt
@@ -8,6 +8,9 @@ import net.apptronic.core.android.anim.transformations.translateYToSelf
 import net.apptronic.core.android.anim.transition.viewTransition
 import net.apptronic.core.demoapp.core.ui.Transitions
 
+/**
+ * Single transition definition.
+ */
 val ViewTransition_SwitchBottomSheet = viewTransition {
     enter(DecelerateInterpolator()) {
         translateYToSelf(1f, 0f)
@@ -19,6 +22,9 @@ val ViewTransition_SwitchBottomSheet = viewTransition {
     }
 }
 
+/**
+ * This object defines custom animation transitions when adding/removing/changing view
+ */
 val AppTransitionFactory = viewTransitionFactory {
     bindTransition(Transitions.ShowHideBottomSheet, ViewTransition_SwitchBottomSheet)
 }
